9f9244b6615698ee6baedc5ebc2bf376230f0c69
Prakhar Prasun <nitunisha2012@gmail.com>
2021-06-04 19:31:34 +0530

    contact form php code fix

--- public/website/form-handler.php
+++ public/website/form-handler.php
@@ -11,10 +11,10 @@ $email_subject = 'New Form Submission';
 $email_body = "User Name: $name.\n".
                  "User Email: $visitor_email.\n".
                    "Subject: $subject.\n".
-                      "User Message: $message.\n";
+                      "User Message: $message .\n";
 
 
-$to = 'pizz123456@outlook.com';
+$to = 'prakharprasun222008@gmail.com';
 
 $headers = "From: $email_from \r\n";
 
@@ -23,4 +23,4 @@ $headers .= "Reply-To: $visitor_email \r\n";
 mail($to,$email_subject,$email_body,$headers);
 
 header("Location: contact.html");
-      ?>
+?>
