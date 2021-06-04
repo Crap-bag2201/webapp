3f8b2457048a55c30c6c26366695d4130f00b794
Prakhar Prasun <nitunisha2012@gmail.com>
2021-06-04 18:54:42 +0530

    contact form small fix

--- public/website/form-handler.php
+++ public/website/form-handler.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_from = 'info@google.com';
+$email_from = 'info@https://temp-prakhar-nodejs.herokuapp.com/contact.html';
 
 $email_subject = 'New Form Submission';
 
